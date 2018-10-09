2ee8f57470e811a91259e9773ead476a2581c93c
HarrisonVanNorth <hgvannort@gmail.com>
2018-10-09 10:30:10 -0700

    finished Form HTML & CSS & remove test from form.js

--- form/form.js
+++
@@ -1,7 +0,0 @@
-axios.get('https://opentdb.com/api.php?amount=10&category=10&difficulty=easy&type=multiple')
-.then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
